0901d1bd7096a8bf34d588d1b8d2ffe704313ab4
kalen510 <kalen.wiley@gmail.com>
2022-05-03 19:50:01 -0400

    prelim user and artist models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Artist = require('./Artist');
+const Post = require('./Post');
+const Sale = require('./Sale')
+
+module.exports = { User, Artist, Post, Sale };
