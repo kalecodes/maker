ef369658efeaabcf028d577f4d94fe0cb4def87a
kalen510 <kalen.wiley@gmail.com>
2022-05-03 23:42:42 -0400

    resolvers wireframe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,48 @@
-
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Post } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        me: {
 
+        },
+        users: {
+
+        },
+        user: {
+            
+        },
+        posts: {
+
+        },
+        post: {
+
+        }
+    },
+    Mutation: {
+        addUser: {
+
+        },
+        login: {
+
+        },
+        addPost: {
+
+        },
+        addLike: {
+            
+        },
+        addComment: {
+
+        },
+        addSave: {
+
+        },
+        addFavorite: {
+            
+        }
+    }
 };
 
 module.exports = resolvers;
