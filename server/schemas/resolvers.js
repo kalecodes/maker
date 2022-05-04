0eb8d4c17112c67e81b63b09782823adfbbcfb67
kalen510 <kalen.wiley@gmail.com>
2022-05-03 23:54:13 -0400

    resolver queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,44 +4,50 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('posts')
+                    .populate('favorites')
+                    .populate('likes')
+                    .populate('saves');
 
-        },
-        users: {
+                return userData;
+            }
 
+            throw new AuthenticationError('Not logged in');
         },
-        user: {
-            
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('posts')
+                .populate('favorites')
+                .populate('likes');
         },
-        posts: {
-
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('posts')
+                .populate('favorites')
+                .populate('likes');
         },
-        post: {
-
-        }
+        posts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Post.find(params).sort({ createdAt: -1 });
+        },
+        post: async (parent, { _id }) => {
+            return Post.findOne({ _id });
+        },
     },
     Mutation: {
-        addUser: {
-
-        },
-        login: {
-
-        },
-        addPost: {
-
-        },
-        addLike: {
-            
-        },
-        addComment: {
-
-        },
-        addSave: {
-
-        },
-        addFavorite: {
-            
-        }
+        addUser: ,
+        login: ,
+        addPost: ,
+        addLike: ,
+        addComment: ,
+        addSave: ,
+        addFavorite: 
     }
 };
 
