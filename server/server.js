d21a891d936aa50d049c9f2cc6250034e2577f0d
kalen510 <kalen.wiley@gmail.com>
2022-05-10 20:31:45 -0400

    add production route

--- server/server.js
+++ server/server.js
@@ -67,9 +67,9 @@ app.use(express.json());
 
 // app.use('/', restRoutes)
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // app.get('*', (req,res) => {
 //     res.sendFile(path.join(__dirname, '../client/build/index.html'));
